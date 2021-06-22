e9e9e3af9e91fc64cd2ba1e3229c993bdf613c05
nicolas cros <nicolas.cros@luos.io>
2021-06-22 17:25:26 +0200

    send a json on a serial port

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,31 @@
 use json::object;
 
-use std::io;
-use std::io::prelude::*;
 use serial::prelude::*;
+use std::io::prelude::*;
 
 fn main() {
-
     // create json command
-    let data_cmd = object!{detection:{}};
+    let data_cmd = object! {detection:{}};
     let json_cmd = json::stringify(data_cmd);
 
+    let luos_json = json_cmd + "\r";
+
     // open serial port
-    let mut port = serial::open("COM2").unwrap();
+    let mut port = serial::open("COM3").unwrap();
 
     // configure port
     port.reconfigure(&|settings| {
-        settings.set_baud_rate(serial::BaudOther(1000000));
+        settings.set_baud_rate(serial::BaudOther(1000000)).unwrap();
         settings.set_char_size(serial::Bits8);
         settings.set_parity(serial::ParityNone);
         settings.set_stop_bits(serial::Stop1);
         settings.set_flow_control(serial::FlowNone);
         Ok(())
-    });
+    })
+    .unwrap();
 
     // send json
-    port.write(json_cmd.as_bytes());
+    port.write(luos_json.as_bytes()).unwrap();
 
     // receive json response
-
-    println!("{}", json_cmd);
 }
